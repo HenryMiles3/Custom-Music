d65bef6b7dd52e757b524926deb9763de5a4e978
HenryMiles3 <henry6miles@gmail.com>
2026-09-23 23:23:42 +0100

    Init

---
+++ src/main/java/io/github/henrymiles3/customMusic/CustomMusic.java
@@ -0,0 +1,16 @@
+package io.github.henrymiles3.customMusic;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CustomMusic extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
